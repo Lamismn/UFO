e0943dca7f323df137c4c2f4c4c2e4f1559c76fb
Lamis <lamis.m.nabil@gmail.com>
2021-05-10 00:23:57 -0400

    updates

--- Static/Js/app.js
+++ Static/Js/app.js
@@ -5,7 +5,7 @@ const tableData = data;
 var tbody = d3.select("tbody");
 function buildTable(data) {
     tbody.html("");
-data.forEach((dataRow) => {
+    data.forEach((dataRow) => {
     let row = tbody.append("tr");
     Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
